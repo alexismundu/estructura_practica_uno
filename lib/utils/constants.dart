b4987c1e56e4f199f6e1c61d226b6a0b068b4501
Alexis Munoz <alexismun53@gmail.com>
2021-02-10 20:20:39 -0600

    Add colors to constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -15,4 +15,80 @@ const String PROFILE_PICTURE =
     "https://dkpp.go.id/wp-content/uploads/2018/10/photo.jpg";
 
 // app
-const Color PRIMARY_COLOR = Colors.indigo;
+const Color PRIMARY_COLOR = MaterialColor(0xFF214254, {
+  50: Color(0xFF214254),
+  100: Color(0xFF214254),
+  200: Color(0xFF214254),
+  300: Color(0xFF214254),
+  400: Color(0xFF214254),
+  500: Color(0xFF214254),
+  600: Color(0xFF214254),
+  700: Color(0xFF214254),
+  800: Color(0xFF214254),
+  900: Color(0xFF214254),
+});
+
+const Color SECONDARY_COLOR = MaterialColor(0xFFFFFFFF, {
+  50: Color(0xFFFFFFFF),
+  100: Color(0xFFFFFFFF),
+  200: Color(0xFFFFFFFF),
+  300: Color(0xFFFFFFFF),
+  400: Color(0xFFFFFFFF),
+  500: Color(0xFFFFFFFF),
+  600: Color(0xFFFFFFFF),
+  700: Color(0xFFFFFFFF),
+  800: Color(0xFFFFFFFF),
+  900: Color(0xFFFFFFFF),
+});
+
+const Color LIST_BACKGROUND_COLOR_DARKER = MaterialColor(0XFF8B8175, {
+  50: Color(0XFF8B8175),
+  100: Color(0XFF8B8175),
+  200: Color(0XFF8B8175),
+  300: Color(0XFF8B8175),
+  400: Color(0XFF8B8175),
+  500: Color(0XFF8B8175),
+  600: Color(0XFF8B8175),
+  700: Color(0XFF8B8175),
+  800: Color(0XFF8B8175),
+  900: Color(0XFF8B8175),
+});
+
+const Color LIST_BACKGROUND_COLOR_LIGHT = MaterialColor(0XFFBCB0A1, {
+  50: Color(0XFFBCB0A1),
+  100: Color(0XFFBCB0A1),
+  200: Color(0XFFBCB0A1),
+  300: Color(0XFFBCB0A1),
+  400: Color(0XFFBCB0A1),
+  500: Color(0XFFBCB0A1),
+  600: Color(0XFFBCB0A1),
+  700: Color(0XFFBCB0A1),
+  800: Color(0XFFBCB0A1),
+  900: Color(0XFFBCB0A1),
+});
+
+const Color DETAILS_IMAGE_BACKGROUND_COLOR_DARKER = MaterialColor(0XFFEC9762, {
+  50: Color(0XFFEC9762),
+  100: Color(0XFFEC9762),
+  200: Color(0XFFEC9762),
+  300: Color(0XFFEC9762),
+  400: Color(0XFFEC9762),
+  500: Color(0XFFEC9762),
+  600: Color(0XFFEC9762),
+  700: Color(0XFFEC9762),
+  800: Color(0XFFEC9762),
+  900: Color(0XFFEC9762),
+});
+
+const Color DETAILS_IMAGE_BACKGROUND_COLOR_LIGHT = MaterialColor(0XFFFABF7C, {
+  50: Color(0XFFFABF7C),
+  100: Color(0XFFFABF7C),
+  200: Color(0XFFFABF7C),
+  300: Color(0XFFFABF7C),
+  400: Color(0XFFFABF7C),
+  500: Color(0XFFFABF7C),
+  600: Color(0XFFFABF7C),
+  700: Color(0XFFFABF7C),
+  800: Color(0XFFFABF7C),
+  900: Color(0XFFFABF7C),
+});
